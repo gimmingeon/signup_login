3634201f19784da0ef42891237f5f0fed6aca3e7
gimmingeon <mnbqwe123987@naver.com>
2024-07-30 01:35:24 +0900

    Feat: 회원가입 기능 추가

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+
+import userRouter from "./routes/users.router.js"
 
 const app = express()
+
 const port = 3000
 
-app.use(bodyParser.json());
+app.use(express.json());
+
+app.use("/users", userRouter);
 
 app.get('/', (req, res)=> {
-    res.send('hello world')
-})
+    res.send('hello world');
+});
 
 app.listen(port, () => {
-    console.log("예시 실행 ${port}")
+    console.log(`예시 실행 ${port}`);
 })
